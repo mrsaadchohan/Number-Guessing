45cafa8dd3abfdba5abb4d5b859e4e16af3bff59
mrsaadchohan <126794253+mrsaadchohan@users.noreply.github.com>
2023-12-24 02:35:54 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,37 @@
+import inquirer from 'inquirer';
+
+
+async function numberguessing() {
+    const sytemnumber= Math.floor(Math.random()*100);
+    const answer= await inquirer.prompt([
+        {
+            type:"Number",
+            name:"guess",
+            message:"Enter number between 1 to 10",
+        }
+
+
+
+
+    ])
+
+
+    const guess= answer.guess;
+console.log(`Your guess is ${guess} \n and system guess is  ${sytemnumber}`)
+    if(guess===sytemnumber)
+    {
+        console.log("Your guess is correct");
+    }
+    else
+    {
+        console.log("Your guess is incorrect try again")
+    }
+};
+
+async function again()
+{
+    
+}
+numberguessing();
+
+
